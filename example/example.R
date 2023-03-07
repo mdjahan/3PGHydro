b30d0b63308546592672a0df7148d7eba89f55fe
mdjahan <122866605+mdjahan@users.noreply.github.com>
2023-03-07 11:16:09 +0100

    Add files via upload

--- example/example.R
+++ example/example.R
@@ -1,36 +1,36 @@
-library(devtools)
-install_github("mdjahan/3PGHydro/rpackage.3PGHydro/")
+#library(devtools)
+#install_github("mdjahan/3PGHydro/rpackage.3PGHydro/")
 library(rpackage.3PGHydro)
 ?run_3PGhydro
 #
-setwd("YOUR_PATH")
+setwd("C:/Users/Marc Djahangard/Documents/IFE/2022/3_3PG/3_3PG_Hydro_R/example")
 
 #climate data
 climate <- read.csv("climate.csv")
 climate$date <- as.Date(climate$date,format="%d/%m/%Y")
 
 #load 3PG parameters
-Parameter <- read.csv("3PG_Parameter.csv") #parameters from Forrester et al. 2021
+Parameter <- read.csv("parameter.csv") #P.abies from Forrester et al. 2021; F. sylvatica from Augustynczik et al 2017
 #select species
-p <- Parameter$Quercus.petraea
+p <- Parameter$Fagus.sylvatica
 
 #Site & Stand characteristics
 lat <- 47.5
 StartDate <- "01/01/1960"
-StandAgei <- 20
+StandAgei <- 30
 EndAge <- 65
-StemNoi <- 500
-WSi <- 200
-WFi <- 10
-WRi <- 20
+StemNoi <- 600
+WSi <- 135
+WFi <- 8
+WRi <- 15
 CO2Concentration <- "Historical"
 FR <- 0.5
 HeightEquation <- 1 
-SVEquation <- 2
+SVEquation <- 1
 #Soil
 SoilClass <- 2
 EffectiveRootZoneDepth <- 1
-DeepRootZoneDepth <- 5
+DeepRootZoneDepth <- 3
 RocksER <- 0.2 
 RocksDR <- 0.4 
 thinAges <- NULL #c(30,40,50,60)
@@ -39,10 +39,25 @@ thinWF <- NULL #c(1,1,1,1)
 thinWR <- NULL #c(1,1,1,1)
 thinWS <- NULL #c(1,1,1,1)
 
+#Output Resolution
 OutputRes <- "daily"
 
-#Run 3PG-Hydro
+#RUN: 3PG-Hydro
 out <- run_3PGhydro(climate,p,lat,StartDate,StandAgei,EndAge,WFi,WRi,WSi,StemNoi,CO2Concentration,FR,SVEquation,HeightEquation,SoilClass,EffectiveRootZoneDepth,DeepRootZoneDepth,RocksER,RocksDR,thinAges,thinVals,thinWF,thinWR,thinWS,OutputRes)
+
+#Some Plots:
+par(mfrow=c(2,2))
+plot(y=out$Height,x=out$Date,main="Height")
+plot(y=out$avDBH,x=out$Date,main="DBH")
+plot(y=out$StandVol,x=out$Date,main="Standing Volume")
+plot(y=out$StemNo,x=out$Date,main="Stems")
+
+par(mfrow=c(2,2))
+plot(out$GPP[730:1095],x=out$Date[730:1095],type="l",main="GPP")
+plot(out$volWCer[730:1095],x=out$Date[730:1095],type="l",main="vol WC ER")
+plot(out$DeepPercolation[730:1095],x=out$Date[730:1095],type="l",main="Deep Percolation")
+plot(out$LAI[730:1095],x=out$Date[730:1095],type="l",main="LAI")
+
 
 #Yearly Output
 OutputRes <- "yearly"
