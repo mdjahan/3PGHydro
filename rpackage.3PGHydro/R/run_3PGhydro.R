89d1d96494d1bf7d1e41db57695ba14b34b65e04
mdjahan <122866605+mdjahan@users.noreply.github.com>
2023-07-20 16:24:14 +0200

    Update run_3PGhydro.R

--- rpackage.3PGHydro/R/run_3PGhydro.R
+++ rpackage.3PGHydro/R/run_3PGhydro.R
@@ -1,7 +1,7 @@
 #' Run 3PG-Hydro
 #'
 #' 3PG-Hydro is an update of the original 3PG Forest Growth model by Landsberg and Waring (1997) (DOI:10.1016/S0378-1127(97)00026-1). 3PG-Hydro calculates on a daily timestep, includes a soil-water-model as well as a snow routine. Further information in the publication on 3PG-Hydro by Yousefpour and Djahangard (2021) (DOI:10.3390/f12121729). 3PG-Hydro is available as an R-package, coding done by Anja Nölte & Marc Djahangard.
-#' @param climate Climate data as .csv file, mandatory column names ("date" ("dd/mm/yyyy"),"Tav","Tmax","Tmin","Rain","SolarRad")
+#' @param climate Climate data as .csv file, mandatory column names ("date" ("dd/mm/yyyy"),"Tav","Tmax","Tmin","Rain","SolarRad", optional:"relHum")
 #' @param p 3PG tree species parameter .csv file
 #' @param lat site latitude UTM
 #' @param StartDate starting date, format: "dd/mm/yyyy"
